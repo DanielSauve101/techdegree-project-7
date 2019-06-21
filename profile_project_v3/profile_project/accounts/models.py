69a6ac7726847d8f7bf5d86238df03ce602d5d89
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-06-21 11:16:54 -0400

    created profile model

--- profile_project_v3/profile_project/accounts/models.py
+++ profile_project_v3/profile_project/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField()
+    bio = models.TextField()
+    avatar = models.ImageField()
+
+    def __str__(self):
+        return self.first_name
