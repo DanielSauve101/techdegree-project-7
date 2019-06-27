0df79186ec951b3f8c828d4eb05fef34afb36375
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-06-27 15:35:54 -0400

    updated Profile model. user field and avatar field.

--- profile_project/accounts/models.py
+++ profile_project/accounts/models.py
@@ -5,13 +5,13 @@ from django.db import models
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     date_of_birth = models.DateField()
     bio = models.TextField()
-    avatar = models.ImageField(upload_to='images/', blank=True)
+    avatar = models.ImageField(default='images/default.jpg', upload_to='images/')
 
     def __str__(self):
         return self.first_name
