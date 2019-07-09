0739c24e020d467ea23af8fa3f43bc6ae1beb9f4
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-07-09 11:30:43 -0400

    changed views.py imports to PEP8 style.

--- profile_project/accounts/views.py
+++ profile_project/accounts/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
+from django.contrib.auth.forms import (AuthenticationForm, UserCreationForm,
+                                       PasswordChangeForm)
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
