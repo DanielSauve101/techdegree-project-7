36a3a528f4a93cf8130f710d0289fd4ddce2c09c
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-07-16 13:03:58 -0400

    added lower() to NewPasswordForm

--- profile_project/accounts/forms.py
+++ profile_project/accounts/forms.py
@@ -36,9 +36,9 @@ class NewPasswordForm(PasswordChangeForm):
         new_password = self.cleaned_data.get('new_password1')
         print('New password:', new_password)
 
-        if self.user.first_name in new_password:
+        if self.user.first_name.lower() in new_password.lower():
             raise forms.ValidationError(
                 'New password cannot contain the user name or parts of the ' +
-                'user’s full name, such as their first name'
+                'user’s full name, such as their first name.'
             )
         return new_password
